33eafd743b9df891be83ed1d16c622e830f76882
SUSAN-MAIDANA <163790129+SUSAN-MAIDANA@users.noreply.github.com>
2024-10-29 16:49:36 -0400

    Delete 08_Tareas/CuartoParcial/Tarea_1 directory

--- 08_Tareas/CuartoParcial/Tarea_1/SUSAN_MAIDANA/ejercicio.js
+++
@@ -1,9 +0,0 @@
-function contarletra(palabra, letra) {
-    let conteo = palabra.toLowerCase().split(letra.toLowerCase()).length - 1;
-    return `La letra '${letra}' aparece ${conteo} veces en la palabra ${palabra}`;
-  }
-
-  //Para la ejecucion: Ingresar datos de entrada
-  console.log(contarletra("suuuuusan", "u")); 
-
-  //la salida nos bota: La letra 'u' aparece 5 veces en la palabra suuuuusan
